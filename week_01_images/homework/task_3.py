90a26f14795148d57caa831edc427cc89b911f0f
Keleas <grishin.na@phystech.edu>
2021-02-08 17:30:43 +0300

    Init 01 lecture and seminar

---
+++ week_01_images/homework/task_3.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+
+def rotate(image, point: tuple, angle: float) -> np.ndarray:
+    """
+    Повернуть изображение по часовой стрелке на угол от 0 до 360 градусов и преобразовать размер изображения.
+
+    :param image: исходное изображение
+    :param point: значение точки (x, y), вокруг которой повернуть изображение
+    :param angle: угол поворота
+    :return: повернутное изображение
+    """
+    # Ваш код
+    pass
+
+    return image
+
+
+def apply_warpAffine(image, points1, points2) -> np.ndarray:
+    """
+    Применить афинное преобразование согласно переходу точек points1 -> points2 и
+    преобразовать размер изображения.
+
+    :param image:
+    :param points1:
+    :param points2:
+    :return: преобразованное изображение
+    """
+    # Ваш код
+    pass
+
+    return image
