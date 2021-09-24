cffb7883e1d58c3980e3df39021336c784fae97a
Nikita Grishin <grishin.na@phystech.edu>
2021-09-24 14:50:06 +0300

    Add 1,2 weeks

---
+++ week_01_images/homework/task_1.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+def find_way_from_maze(image: np.ndarray) -> tuple:
+    """
+    Найти путь через лабиринт.
+
+    :param image: изображение лабиринта
+    :return: координаты пути из лабиринта в виде (x, y), где x и y - это массивы координат
+    """
+    coords = None
+    # Ваш код тут
+    pass
+    # Ваш код тут
+
+    return coords
