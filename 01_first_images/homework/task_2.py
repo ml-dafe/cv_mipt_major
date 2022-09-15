2f678a1f6bf70d3cb5be48f2d0de258ab1c6850d
Nikita Grishin <grishin.na@phystech.edu>
2022-09-15 21:08:59 +0300

    add 01 week

---
+++ 01_first_images/homework/task_2.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+def find_road_number(image: np.ndarray) -> int:
+    """
+    Найти номер дороги, на которой нет препятсвия в конце пути.
+
+    :param image: исходное изображение
+    :return: номер дороги, на котором нет препятсвия на дороге
+    """
+    road_number = None
+    # Ваш код тут
+    pass
+    # Ваш код тут
+
+    return road_number
